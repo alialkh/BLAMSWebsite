e57af772a6b49bf9de7e35fd658ff8cf956c4998
alialkh <32747088+alialkh@users.noreply.github.com>
2023-10-18 14:22:22 -0600

    HEADER IN CAPS

--- js/customElements.js
+++ js/customElements.js
@@ -62,7 +62,7 @@ class customHeader extends HTMLElement {
         <header class="header" style="background-color: #e3f2fd;">
     <nav class="navbar navbar-expand-lg navbar-light" >
         <div class="container">
-            <div class="flex-grow-1"><a class="navbar-brand monteserrat" href="index.html" style="font-size:3em">AAMEH Law</a> </div>
+            <div class="flex-grow-1"><a class="navbar-brand monteserrat" href="index.html" style="font-size:3em">AAMEH LAW FIRM</a> </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
